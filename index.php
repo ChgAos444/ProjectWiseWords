63bab620b3f091f3f2bea41f59987412619cbe56
Arens <arensidani@gmail.com>
2025-12-23 14:44:01 +0100

    nav - version 1.0

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <?php
-    echo "<p> Hello World! </p>"
+    require_once "parts/header.php";
+    require_once "parts/nav.php";
+
+    echo "<p> Hello World! </p>";
+
+    require_once "parts/footer.php";
 ?>
---
+++ parts/nav.php
@@ -0,0 +1,53 @@
+<style>
+    .nav-link {
+        color: #f8f9fa;
+        font-weight: bold;
+    }
+
+    .nav-link:hover {
+        color: #9A3F3F;
+        font-weight: bold;
+    }
+
+    .patrick-hand-regular {
+        font-family: "Patrick Hand", cursive;
+        font-weight: 400;
+        font-style: normal;
+        font-size: 15pt;
+    }
+</style>
+
+<nav class="navbar navbar-expand-lg patrick-hand-regular" style="background-color: #FD7979;" data-bs-theme="light">
+    <div class="container-fluid mx-5">
+        <a class="navbar-brand" href="#">
+            <img src="http://localhost/ProjectWiseWords/images/project_logo.png" class="img-fluid my-1"
+                 style="height: 2.5rem;" alt="logo">
+        </a>
+
+        <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" style="color: #9A3F3F;"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="d-lg-flex justify-content-end collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0 d-md-flex justify-content-between" style="width: 40%">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Video</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Artikuj</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Librari</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Rreth nesh</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kontakt</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
